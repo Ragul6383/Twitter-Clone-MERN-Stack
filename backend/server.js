5b8c5b2e3b50f6bb6f6fb28ff5ea9cd50f7bcf69
Ragul <ragulr034@example.com>
2025-04-01 02:08:11 +0530

    1-auth controllers added

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
+
 import authRoutes from "./routes/auth.routes.js";
 import connectMongoDB from "./db/connectMongoDB.js";
+import cookieParser from "cookie-parser";
 
 
 dotenv.config();
@@ -9,6 +11,10 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(express.json());
+app.use(express.urlencoded({extended:true})); // for parsing application/x-www-form-urlencoded
+
+app.use(cookieParser()); 
 
 app.use("/api/auth",authRoutes);
 
