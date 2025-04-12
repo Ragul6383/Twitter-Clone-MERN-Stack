7bd7119e8f2aa6b414d60c4c29a9f5f6f709d0ad
Ragul <ragulr034@example.com>
2025-04-12 22:16:01 +0530

    3- profile page posts and user info fetched

--- frontend/src/components/common/Post.jsx
+++ frontend/src/components/common/Post.jsx
@@ -6,7 +6,7 @@ import { FaTrash } from "react-icons/fa";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
-import { toast } from "react-hot-toast";
+import { toast }  from "react-hot-toast";
 
 import LoadingSpinner from "./LoadingSpinner";
 import { formatPostDate } from "../../utils/date";
